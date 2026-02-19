fa96ecd7b0596fdc07deb2703399e65ca72d5247
ohhGHST <salikluis@outlook.com>
2026-02-19 11:00:24 -0600

    feat: define filesystem paths in flipper_animation_switcher.h

---
+++ flipper_animation_switcher.h
@@ -0,0 +1,4 @@
+/* ── Filesystem paths ─────────────────────────────────────────────────── */
+#define FAS_DOLPHIN_PATH   "/ext/dolphin"
+#define FAS_PLAYLISTS_PATH "/ext/apps_data/flipper_animation_switcher"
+#define FAS_MANIFEST_PATH  "/ext/dolphin/manifest.txt"
