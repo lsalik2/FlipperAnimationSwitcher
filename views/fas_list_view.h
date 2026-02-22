5cf59b67319d37bd94f2aa00720a511fd0e0184a
ohhGHST <salikluis@outlook.com>
2026-02-21 18:31:04 -0600

    feat: add maximum list items and user callback events

--- views/fas_list_view.h
+++ views/fas_list_view.h
@@ -2,3 +2,14 @@
 
 #include <gui/view.h>
 #include <stdbool.h>
+
+/* Maximum items the list can hold */
+#define FAS_LIST_MAX_ITEMS 128 // TODO increase this if possible
+#define FAS_LIST_LABEL_LEN  64
+
+/* Events fired via the user callback */
+typedef enum {
+    FasListEvtOkShort,  /* OK pressed briefly (toggles checkbox if present) */
+    FasListEvtOkLong,   /* OK held                                          */
+    FasListEvtRight,    /* Right arrow pressed                              */
+} FasListEvent;
