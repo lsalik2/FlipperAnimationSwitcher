4c9e895e5bddeec99e6f0facf6e34f156d9372ed
ohhGHST <salikluis@outlook.com>
2026-02-21 18:40:38 -0600

    feat: add FasMainMenuIdx enum to fas_scene_main_menu.c

---
+++ scenes/fas_scene_main_menu.c
@@ -0,0 +1,9 @@
+#include "../flipper_animation_switcher.h"
+#include "fas_scene.h"
+
+typedef enum {
+    FasMainIdxCreate = 0,
+    FasMainIdxChoose,
+    FasMainIdxDelete,
+    FasMainIdxAbout,
+} FasMainMenuIdx;
