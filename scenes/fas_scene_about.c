389ca9396ee19d5a68be31a3898cdb6963800855
ohhGHST <salikluis@outlook.com>
2026-02-22 23:48:57 -0600

    feat: add about section credits

---
+++ scenes/fas_scene_about.c
@@ -0,0 +1,6 @@
+#include "../flipper_animation_switcher.h"
+#include "fas_scene.h"
+
+#define FAS_VERSION   "1.0.0"
+#define FAS_AUTHOR    "SLK"
+#define FAS_REPO      "github.com/lsalik2/FlipperAnimationSwitcher"
